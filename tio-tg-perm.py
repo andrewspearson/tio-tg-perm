9d45582c1e307445811e56937795f16537ff35a8
Andrew Pearson <76594022+andrewspearson@users.noreply.github.com>
2021-02-02 17:56:23 -0500

    Update tio-tg-perm.py

--- tio-tg-perm.py
+++ tio-tg-perm.py
@@ -7,7 +7,7 @@ tio_client = TenableIO(
 )
 
 session_details = tio_client.session.details()
-if session_details['permissions'] != 64 or 'ADMIN' not in session_details['user_permissions']:
+if session_details['permissions'] != 64:
     print('This script must run as a user with Administrator permissions. Exiting.')
     quit()
 
@@ -24,13 +24,13 @@ for tg in tio_client.target_groups.list():
             print('Updating System Target Group ' + tg['name'] + ' Default permission to \'Can Use\'')
             default_can_use_acl[0]['permissions'] = 32
             tio_client.target_groups.edit(tg['id'], acls=default_can_use_acl)
-        elif (tg['type'] == 'system' and tg['name'] == 'Default') or tg['type'] == 'group':
-            pass
         elif tg['type'] == 'user':
             print('Updating User Target Group ' + tg['name'] + ' Default permission to \'Can Use\'')
             # 16 is the permission to set a User Target Group to "Can Use". 32 will set the permission to "Can Change"
             default_can_use_acl[0]['permissions'] = 16
             tio_client.target_groups.edit(tg['id'], acls=default_can_use_acl)
+        elif (tg['type'] == 'system' and tg['name'] == 'Default') or tg['type'] == 'group':
+            pass
         else:
             print('WARNING: Unrecognized Target Group type.')
             print(tg['type'])
