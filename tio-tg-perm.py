54289ab4551d95f169ca570ec8e41ddea1f7b7a8
Andrew Pearson <76594022+andrewspearson@users.noreply.github.com>
2021-02-02 17:29:45 -0500

    Update tio-tg-perm.py

--- tio-tg-perm.py
+++ tio-tg-perm.py
@@ -5,7 +5,6 @@ tio_client = TenableIO(
     secret_key='',
     proxies={}
 )
-# tio_client._session.verify = False
 
 session_details = tio_client.session.details()
 if session_details['permissions'] != 64 or 'ADMIN' not in session_details['user_permissions']:
@@ -20,17 +19,21 @@ default_can_use_acl = [
     }
 ]
 for tg in tio_client.target_groups.list():
-    if tg['type'] == 'system' and tg['name'] != 'Default':
-        print('Updating System Target Group ' + tg['name'] + ' Default permission to \'Can Use\'')
-        default_can_use_acl[0]['permissions'] = 32
-        tio_client.target_groups.edit(tg['id'], acls=default_can_use_acl)
-    elif (tg['type'] == 'system' and tg['name'] == 'Default') or tg['type'] == 'group':
+    try:
+        if tg['type'] == 'system' and tg['name'] != 'Default':
+            print('Updating System Target Group ' + tg['name'] + ' Default permission to \'Can Use\'')
+            default_can_use_acl[0]['permissions'] = 32
+            tio_client.target_groups.edit(tg['id'], acls=default_can_use_acl)
+        elif (tg['type'] == 'system' and tg['name'] == 'Default') or tg['type'] == 'group':
+            pass
+        elif tg['type'] == 'user':
+            print('Updating User Target Group ' + tg['name'] + ' Default permission to \'Can Use\'')
+            # 16 is the permission to set a User Target Group to "Can Use". 32 will set the permission to "Can Change"
+            default_can_use_acl[0]['permissions'] = 16
+            tio_client.target_groups.edit(tg['id'], acls=default_can_use_acl)
+        else:
+            print('WARNING: Unrecognized Target Group type.')
+            print(tg['type'])
+    # Generic pass for any unforeseen errors
+    except:
         pass
-    elif tg['type'] == 'user':
-        print('Updating User Target Group ' + tg['name'] + ' Default permission to \'Can Use\'')
-        # 16 is the permission to set a User Target Group to "Can Use". 32 will set the permission to "Can Change"
-        default_can_use_acl[0]['permissions'] = 16
-        tio_client.target_groups.edit(tg['id'], acls=default_can_use_acl)
-    else:
-        print('WARNING: Unrecognized Target Group type.')
-        print(tg['type'])
